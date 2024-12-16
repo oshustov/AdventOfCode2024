c7833cab11cb55229ed3dfe174ce1dfe429493a9
o.shustov <shustov.sanya@gmail.com>
2024-12-16 21:41:48 +0200

    day5 part 1 naive solution

--- Day5/Day5_SafetyManual.cs
+++ Day5/Day5_SafetyManual.cs
@@ -7,36 +7,68 @@ public static class Day5_SafetyManual
     var file = File.OpenRead(Path.Combine("Day5", "Input.txt"));
     var reader = new StreamReader(file);
 
-    var rules = new List<Rule>();
-    var updates = new List<Update>();
+    var rules = new Dictionary<int, List<int>>();
+    var updates = new List<Dictionary<int, int>>();
     while (!reader.EndOfStream)
     {
-      Action action = MatchKind(reader.ReadLine()) switch
-      {
-        { Rule: not null } x => () => rules.Add(x.Rule),
-        { Update: not null } x => () => updates.Add(x.Update),
-        _ => () => { }
-      };
-      action.Invoke();
+      var kind = MatchKind(reader.ReadLine());
+      if (kind is { Update: not null })
+        updates.Add(kind.Update.Pages
+          .Select((page, index) => (page, index))
+          .ToDictionary(tuple => tuple.page, tuple => tuple.index));
+
+      if (kind is not {Rule: not null})
+        continue;
+
+      if (!rules.ContainsKey(kind.Rule.Number))
+        rules.Add(kind.Rule.Number, []);
+
+      rules[kind.Rule.Number].Add(kind.Rule.ShouldBeBeforeNumber);
     }
 
-    foreach (var update in updates)
+    var validUpdates = new List<int[]>();
+    foreach (var updateDictionary in updates)
     {
-      var 
+      var isValid = true;
+      foreach (var (pageNumber, pageIndex) in updateDictionary)
+      {
+        var pageRules = rules.GetValueOrDefault(pageNumber, []);
+        if (pageRules.Any(x => updateDictionary.GetValueOrDefault(x, int.MaxValue) < pageIndex))
+        {
+          isValid = false;
+          break;
+        }
+      }
+
+      if (isValid)
+        validUpdates.Add(CopyToArray(updateDictionary));
     }
-    
+
+    var result = validUpdates.Sum(x => x.ElementAt((x.Length / 2)));
+
     Console.WriteLine($"Rules count: {rules.Count}");
     Console.WriteLine($"Updates count: {updates.Count}");
+    Console.WriteLine($"Invalid updates count: {validUpdates.Count}");
+    Console.WriteLine($"Result sum is: {result}");
     Console.ReadLine();
   }
-  
+
+  public static int[] CopyToArray(Dictionary<int, int> dictionary)
+  {
+    var array = new int[dictionary.Count];
+    foreach (var (page, index) in dictionary)
+      array[index] = page;
+
+    return array;
+  }
+
   public static (Rule? Rule, Update? Update) MatchKind(string self) =>
     self switch
     {
       not null when self.Contains('|') => (Rule.From(self.Split("|").ToArray()), default),
       not null when self.Contains(',') => (default, Update.From(self.Split(","))),
       _ => default
-    };
+    }; 
 }
 
 public record Rule(int Number, int ShouldBeBeforeNumber)
@@ -55,27 +87,5 @@ public record Update(int[] Pages)
   public static Update From(string[] parts)
   {
     return new Update(parts.Select(int.Parse).ToArray());
-  }
-}
-
-public class RulesTree
-{
-  public record Node(int Num, Node[] Next);
-  
-  public Node Root { get; set; }
-
-  public int[] GetBeforeNumbers(int number)
-  {
-    if (Root.Num == number)
-      return [];
-    
-    var current = Root;
-    while (current.Next != null)
-    {
-      foreach (var nextNode in current.Next)
-      {
-        //todo
-      }
-    }
   }
 }
